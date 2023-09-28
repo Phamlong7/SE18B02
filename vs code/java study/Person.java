3344ecf798553154d6f2d2d4fb19d4aacd61fc43
Phamlong7 <88232978+Phamlong7@users.noreply.github.com>
2023-09-28 22:18:51 +0700

    Create Person.java

---
+++ vs code/java study/Person.java
@@ -0,0 +1,46 @@
+public class Person {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+    private String gender;
+    private String phoneNumber;
+    private String address;
+
+    public Person(String email, String firstName, String lastName, int birthYear, String gender, String phoneNumber, String address) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Email: " + email);
+        System.out.println("Birth Year: " + birthYear);
+        System.out.println("Gender: " + gender);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Address: " + address);
+    }
+
+    public void changePhoneNumber(String newPhoneNumber) {
+        this.phoneNumber = newPhoneNumber;
+    }
+
+    public void changeAddress(String newAddress) {
+        this.address = newAddress;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Long@example.com", "Pham", "Long", 2004, "Male", "0868772004", "123 Main St");
+        System.out.println("Personal Information:");
+        person.displayInfo();
+        person.changePhoneNumber("0868772005");
+        person.changeAddress("456 Elm St");
+        System.out.println("\nUpdated Information:");
+        person.displayInfo();
+    }
+}
